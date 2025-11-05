3165e96562c357a8c2ca434f2010ce54c28403ac
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-05 19:00:13 +0100

    8-square.js add col and row to the logic

--- 8-square.js
+++ 8-square.js
@@ -5,6 +5,8 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size.length; i++) {
-    console.log(X.repeat(size));
+    let col = X.repeat(size);
+    let row = col + '\n';
+    console.log(row.repeat(size));
   }
 }
