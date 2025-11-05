19219208a50fb92258371bde16ca869674fbffda
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-05 19:04:33 +0100

    Removed the col and row

--- 8-square.js
+++ 8-square.js
@@ -4,9 +4,7 @@ let X = 'X';
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size.length; i++) {
-    let col = X.repeat(size);
-    let row = col + '\n';
-    console.log(row.repeat(size));
+  for (let i = 0; i < size; i++) {
+    console.log(X.repeat(size));
   }
 }
