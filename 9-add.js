387fe5ff2468a58310b09f98af13741fb99b8b14
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-05 19:21:28 +0100

    Replaced NAN with NaN

--- 9-add.js
+++ 9-add.js
@@ -3,7 +3,7 @@ let b = process.argv[3];
 
 function add(a, b) {
   if (isNaN(a) && isNaN(b)) {
-    console.log('NAN');
+    console.log('NaN');
   } else {
     console.log(Number(a) + Number(b));
   }
